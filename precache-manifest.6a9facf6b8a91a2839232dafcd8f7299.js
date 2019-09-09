646808ed11bfc37cfc8b2f93b2ef67d84d30573f
Neel Patel <321neel@gmail.com>
2019-09-09 15:56:17 -0400

    Updates

---
+++ precache-manifest.6a9facf6b8a91a2839232dafcd8f7299.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42750394461d3022b820d46c57e5ab25",
+    "url": "/projectFiveReact/index.html"
+  },
+  {
+    "revision": "90cb36cdbc9ec6e8aa6f",
+    "url": "/projectFiveReact/static/css/2.f925f819.chunk.css"
+  },
+  {
+    "revision": "ee272e4eae167417cf7c",
+    "url": "/projectFiveReact/static/css/main.f72b37c3.chunk.css"
+  },
+  {
+    "revision": "90cb36cdbc9ec6e8aa6f",
+    "url": "/projectFiveReact/static/js/2.2cfe4298.chunk.js"
+  },
+  {
+    "revision": "ee272e4eae167417cf7c",
+    "url": "/projectFiveReact/static/js/main.ca5fba0b.chunk.js"
+  },
+  {
+    "revision": "59c88dde8515912d32f0",
+    "url": "/projectFiveReact/static/js/runtime~main.e978d71c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/projectFiveReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/projectFiveReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/projectFiveReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/projectFiveReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/projectFiveReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
