c4d6b1513a3b8aaa3a312b58aed35945a6d79af0
Neel Patel <321neel@gmail.com>
2019-09-09 00:41:06 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import StockChart from './StockChart'
-import Plot from 'react-plotly.js';
+// import Banner from './Banner';
 import { Line } from 'react-chartjs-2';
 import './App.scss';
+import { thisExpression } from '@babel/types';
 
 
 
@@ -12,8 +12,19 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      activeCompanyOne: "",
+      activeCompanyTwo: "",
+      activeCompanyThree: "",
+      activeCompanyFour: "",
+      activeCompanyFive: "",
+      activeCompanySix: "",
+      activeCompanySeven: "",
+      activeCompanyEight: "",
+      activeCompanyNine: "",
+      activeCompanyTen: "",
+
       companyTitle:"Facebook Inc.",
-      mainPercentChange: '0.00%',
+      mainPercentChange: '0.00',
       stockQuote: [],
       autoFill: [],
       userInput: '',
@@ -32,6 +43,115 @@ class App extends Component {
         ],
       }
     }
+  }
+
+  componentDidMount(){
+    axios({
+      method: 'GET',
+      url: `https://financialmodelingprep.com/api/v3/stock/actives`,
+      dataResponse: 'json',
+    }).then(result => {
+
+      result = result.data
+
+      let activeArray = result["mostActiveStock"];
+
+      this.setState({
+        activeCompanyOne: activeArray[0],
+        activeCompanyTwo: activeArray[1],
+        activeCompanyThree: activeArray[2],
+        activeCompanyFour: activeArray[3],
+        activeCompanyFive: activeArray[4],
+        activeCompanySix: activeArray[5],
+        activeCompanySeven: activeArray[6],
+        activeCompanyEight: activeArray[7],
+        activeCompanyNine: activeArray[8],
+        activeCompanyTen: activeArray[9],
+      })
+
+    })
+    axios({
+      method: 'GET',
+      url: `https://www.alphavantage.co/query?/`,
+      dataResponse: 'json',
+      params: {
+        function: 'GLOBAL_QUOTE',
+        symbol: 'FB',
+        apikey: "WPZV8PD9NGMXNUEF",
+        format: "json",
+
+      },
+    }).then(res => {
+      //res all the information for the searched stock ticker
+      res = res.data['Global Quote'];
+
+      this.setState({
+        stockQuote: Object.entries(res),
+        userInput: "",
+      })
+
+      axios({
+        method: 'GET',
+        url: `https://www.alphavantage.co/query?/`,
+        dataResponse: 'json',
+        params: {
+          function: 'TIME_SERIES_INTRADAY',
+          symbol: 'FB',
+          apikey: "WPZV8PD9NGMXNUEF",
+          interval: '5min',
+          format: "json",
+        },
+
+      }).then(result => {
+        result = result.data["Time Series (5min)"];
+        console.log(result);
+        let datesArray = [];
+        let pricesArray = [];
+
+        for (let i in result) {
+          datesArray.push(i)
+          pricesArray.push(result[i]["4. close"])
+        }
+
+        let arrayDataSets = [{
+          label: '',
+          data: pricesArray,
+        }]
+
+        let myChartData = {
+          arrayDataSets,
+          pricesArray
+        }
+
+        this.state.chartData.datasets[0].label = 'FB';
+        this.state.chartData.labels = datesArray.reverse();
+        this.state.chartData.datasets[0].data = pricesArray.reverse();
+
+        this.setState({
+          // chartData: myChartData
+          state: this.state,
+        })
+
+      })
+
+
+      axios({
+        method: 'GET',
+        url: `https://financialmodelingprep.com/api/v3/company/profile/FB`,
+        dataResponse: 'json',
+      }).then(result => {
+
+        result = result.data['profile'];
+
+        let percentString = result['changesPercentage'].replace(/[!@#$%^&*()]/g, "");
+
+        this.setState({
+          companyTitle: result['companyName'],
+          mainPercentChange: percentString,
+        })
+
+      })
+    })
   }
 
 
@@ -83,7 +203,6 @@ class App extends Component {
     event.preventDefault()
 
     let symbol = this.state.userInput
-    alert(symbol)
 
     if (symbol !==''){
       axios({
@@ -103,7 +222,7 @@ class App extends Component {
 
         this.setState({
           stockQuote: Object.entries(res),
-          // userInput: "",
+          userInput: "",
         })
         console.log(Object.entries(res))
 
@@ -185,8 +304,6 @@ class App extends Component {
 
 
   render() {
-    let stockQuoteArray = this.state.stockQuote;
-
 
     return (
       <div className="App">
@@ -212,48 +329,80 @@ class App extends Component {
               }
             </ul>
           </div>
-
           <div className="stockPortfolio">
-
           </div>
-
-
         </nav>
         
+
+
+
         <main>
           <div className='banner'>
-            <ul className="dailyWinnersLosers">
-                  <li>
-                    <h4>Stock Name</h4>
-                    <p>price<span> <i className="fa fa-caret-up"></i></span></p>
-                  </li>
+            <ul className="activeList">
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyOne["ticker"]}</h4>
+                <p>{this.state.activeCompanyOne["price"]} (<span style={{ color: this.state.activeCompanyOne["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyOne["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyTwo["ticker"]}</h4>
+                <p>{this.state.activeCompanyTwo["price"]} (<span style={{ color: this.state.activeCompanyTwo["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyTwo["changes"]}</span>)</p>
+              </li>
 
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyThree["ticker"]}</h4>
+                <p>{this.state.activeCompanyThree["price"]} (<span style={{ color: this.state.activeCompanyThree["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyThree["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyFour["ticker"]}</h4>
+                <p>{this.state.activeCompanyFour["price"]} (<span style={{ color: this.state.activeCompanyFour["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyFour["changes"]}</span>)</p>
+              </li>
+
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyFive["ticker"]}</h4>
+                <p>{this.state.activeCompanyFive["price"]} (<span style={{ color: this.state.activeCompanyFive["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyFive["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanySix["ticker"]}</h4>
+                <p>{this.state.activeCompanySix["price"]} (<span style={{ color: this.state.activeCompanySix["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanySix["changes"]}</span>)</p>
+              </li>
+
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanySeven["ticker"]}</h4>
+                <p>{this.state.activeCompanySeven["price"]} (<span style={{ color: this.state.activeCompanySeven["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanySeven["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyEight["ticker"]}</h4>
+                <p>{this.state.activeCompanyEight["price"]} (<span style={{ color: this.state.activeCompanyEight["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyEight["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyNine["ticker"]}</h4>
+                <p>{this.state.activeCompanyNine["price"]} (<span style={{ color: this.state.activeCompanyNine["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyNine["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyNine["ticker"]}</h4>
+                <p>{this.state.activeCompanyNine["price"]} (<span style={{ color: this.state.activeCompanyNine["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyNine["changes"]}</span>)</p>
+              </li>
+              <li className="activeContainer">
+                <h4>{this.state.activeCompanyTen["ticker"]}</h4>
+                <p>{this.state.activeCompanyTen["price"]} (<span style={{ color: this.state.activeCompanyTen["changes"] > 0 ? 'green' : 'red' }}>{this.state.activeCompanyTen["changes"]}</span>)</p>
+              </li>
             </ul>
+
           </div>
 
 
           <div className="wrapper chartNumbersInfo">
             
-            <h2>{this.state.companyTitle} (<span style={{ color: this.state.mainPercentChange > 0 ? 'green' : 'red'}}>{this.state.mainPercentChange}</span>) </h2>
+            <h2>{this.state.companyTitle} (<span style={{ color: this.state.mainPercentChange > 0 ? 'green' : 'red'}}>{this.state.mainPercentChange}%</span>) </h2>
 
             <div className="numbersGraph">
               <div className="companyNumbers">
-                <ul className="stockInformationPlaceHolders">
-                  <li>Ticker: </li>
-                  <li>Open: </li>
-                  <li>High: </li>
-                  <li>Low: </li>
-                  <li>Price: </li>
-                  <li>Volume: </li>
-                  <li>Day: </li>
-                  <li>Previous Close: </li>
-                  <li>($)Change: </li>
-                  <li>(%)Change: </li>
-                </ul>
+                
                 <ul className="stockInformation">
                   {
-                    stockQuoteArray.map((value, index) => {
-                      return <li key={index}><span>{value[1]}</span> </li>
+                    this.state.stockQuote.map((value, index) => {
+                      return <li key={index}><span>{value[0]}</span> <span>{value[1]}</span></li>
+                      
                     })
                   }
                 </ul>
@@ -261,9 +410,9 @@ class App extends Component {
               <div className="stockChart">
                 <Line
                   data={this.state.chartData}
-                  width={700}
-                  height={450}
-                  options={{}}
+                  width={800}
+                  height={500}
+                  options={{ responsive: true,}}
                 />
               </div>
             </div>
