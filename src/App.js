071f3ca9277417ce842511219bf9a21eb1711127
Neel Patel <321neel@gmail.com>
2019-09-10 12:28:05 -0400

    added some background animations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 // import Banner from './Banner';
 import { Line } from 'react-chartjs-2';
+import Particles from 'react-particles-js';
 import './App.scss';
 
 
@@ -354,6 +355,59 @@ class App extends Component {
 
     return (
       <div className="App">
+        <div className="particles">
+          <Particles params={{
+            "particles": {
+              "number": {
+                "value": 160,
+                "density": {
+                  "enable": false
+                }
+              },
+              "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                  "speed": 4,
+                  "size_min": 0.3
+                }
+              },
+              "line_linked": {
+                "enable": false
+              },
+              "move": {
+                "random": true,
+                "speed": 1,
+                "direction": "top",
+                "out_mode": "out"
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "bubble"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "repulse"
+                }
+              },
+              "modes": {
+                "bubble": {
+                  "distance": 250,
+                  "duration": 2,
+                  "size": 0,
+                  "opacity": 0
+                },
+                "repulse": {
+                  "distance": 400,
+                  "duration": 4
+                }
+              }
+            }
+          }} />
+        </div>
         <div className="mainContentWrapper">
           <nav>
             <div className="title">
